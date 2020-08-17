8c2f32a6701e624384a824415deb6e40803d58ff
pankajks5019 <62767172+pankajks5019@users.noreply.github.com>
2020-08-17 16:58:48 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ResearchComponent } from './research/research.component';
+import { LatestComponent } from './latest/latest.component';
+import { ProjectComponent } from './project/project.component';
+import { PublicationComponent } from './publication/publication.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',redirectTo: 'home',pathMatch: 'full'
+  },
+  {
+    path: 'home',component: HomeComponent
+  },
+  {
+    path: 'profile',component: ProfileComponent
+  },
+  {
+    path: 'research',component: ResearchComponent
+  },
+  {
+    path: 'latest',component: LatestComponent
+  },
+  {
+    path: 'project',component: ProjectComponent
+  },
+  {
+    path: 'publication',component: PublicationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { ContentCardComponent } from './content-card/content-card.component';
+import { ResearchComponent } from './research/research.component';
+import { LatestComponent } from './latest/latest.component';
+import { PublicationComponent } from './publication/publication.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    HomeComponent,
+    ContentCardComponent,
+    ResearchComponent,
+    LatestComponent,
+    PublicationComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
